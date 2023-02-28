5dafc5f1af7dc3b97d7a75cddb3788137947fa33
neon <neon@gmail.com>
2023-02-28 22:17:46 +0900

    js

---
+++ main.js
@@ -0,0 +1,20 @@
+//первое задание
+var word = prompt("ваше имя")
+console.log("меня зовут " + word);
+var word = prompt("введите возраст");
+console.log("мне "+ word + " лет");
+var number = prompt("решите пример 6+3=")
+console.log("Пример 1: "+"6+3="+(6+3)+" Ваш ответ "+ number);
+var number = prompt("решите пример 6-3=")
+console.log("Пример 2: "+"6-3="+(6-3)+" Ваш ответ "+ number);
+var number = prompt("решите пример 6*3=")
+console.log("Пример 3: "+"6*3="+(6*3)+" Ваш ответ "+ number);
+var number = prompt("решите пример 6/3=")
+console.log("Пример 4: "+"6/3="+(6/3)+" Ваш ответ "+ number);
+var number = prompt("решите пример 6%3=")
+console.log("Пример 5: "+"6%3="+(6%3)+" Ваш ответ "+ number);
+//второе задание
+var first = +prompt("введите первое число")
+var second = +prompt("введите второе число")
+var third = +prompt("введите третье число")
+alert(((first + second + third)/3))
